3f301027b4680024ad84026a377039f9e1516964
Vikki1312 <nikitchenkovika48@gmail.com>
2023-05-16 18:15:14 +0700

    add homework 1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const markup = createImgCards(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+galleryEl.style.listStyle = 'none';
+
+function createImgCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const VIDEO_STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveCurrentTimeVideo, 1000));
+updateCurrentTimeVideo();
+
+function saveCurrentTimeVideo(data) {
+  console.log(data.seconds);
+  localStorage.setItem(VIDEO_STORAGE_KEY, data.seconds);
+}
+
+function updateCurrentTimeVideo() {
+  const persistedData = localStorage.getItem(VIDEO_STORAGE_KEY);
+
+  if (!persistedData) {
+    return;
+  }
+  player.setCurrentTime(persistedData);
+}
