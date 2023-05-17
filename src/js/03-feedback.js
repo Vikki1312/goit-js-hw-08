01a88c145993fdeb2571e4d70dff22af76b09d2d
Vikki1312 <nikitchenkovika48@gmail.com>
2023-05-17 17:53:39 +0700

    add homework 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('.feedback-form input'),
+};
+
+refs.input.setAttribute('required', '');
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormDate, 500));
+
+let formData = {};
+
+populateTextarea();
+
+function onFormDate(evt) {
+  formData.email = refs.input.value;
+  formData.message = refs.textarea.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parsedSaveMessage = JSON.parse(savedMessage);
+  console.log(parsedSaveMessage);
+  if (savedMessage) {
+    refs.input.value = parsedSaveMessage.email;
+    refs.textarea.value = parsedSaveMessage.message;
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(formData);
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
